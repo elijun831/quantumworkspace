5db9a08678860e46b4dcb982e32643b8fd6713ce
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-03-10 12:58:10 +0900

    Update jupyter_notebook_config.py

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -2,7 +2,7 @@ c = get_config()
 
 # Set options for Jupyter Notebook
 c.NotebookApp.token = ''
-# c.NotebookApp.password = ''  # Commented out to leave the password blank
+# c.NotebookApp.password = ''  # Commented out to leave the password blank; you can uncomment this line and set the password if you wish to do so
 c.NotebookApp.allow_root = True  # Allow the notebook server to be run by the root user
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
