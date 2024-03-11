dde0a78ef85be0e899d82a5f5f3530283e886c33
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-03-11 23:38:00 +0900

    Update jupyter_notebook_config.py

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -1,16 +1,31 @@
-c = get_config()
-
-# Set options for Jupyter Notebook
+# Common settings for both Jupyter Notebook and JupyterLab
 c.NotebookApp.token = ''
-# c.NotebookApp.password = ''  # Commented out to leave the password blank; you can uncomment this line and set the password if you wish to do so
-c.NotebookApp.allow_root = True  # Allow the notebook server to be run by the root user
+# c.NotebookApp.password = ''  # Commented out to leave the password blank
+c.NotebookApp.allow_root = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.open_browser = False
+c.ServerApp.trust_xheaders = True
 
-# Set options for JupyterLab
+# Settings for JupyterLab
 c.ServerApp.jpserver_extensions = {
     'jupyterlab': True,
     'other_extension': True,
 }
+
+# Enable WebSocket with SSL
+c.NotebookApp.websocket_url = 'wss://127.0.0.1:8888'
+c.NotebookApp.base_url = '/'
+
+# Adjust Content-Security-Policy for secure WebSocket connection
+c.NotebookApp.webapp_settings = {
+    'headers': {
+        'Content-Security-Policy': "frame-ancestors * 'self';"
+    }
+}
+
+# SSL/TLS settings
+c.NotebookApp.allow_origin = 'https://*.quantumworkspace.dev'
+c.NotebookApp.allow_origin_pat = 'https://.*.quantumworkspace.dev'
+c.NotebookApp.disable_check_xsrf = True
